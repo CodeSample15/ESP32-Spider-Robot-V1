c684c5d898c3fc0fadda04c582836da74e756d9d
CodeSample15 <lukecrimi15@gmail.com>
2026-01-04 15:25:37 -0500

    Test code for calibration

--- YASBot/Motor.cpp
+++ YASBot/Motor.cpp
@@ -6,3 +6,24 @@ Motor::Motor(uint8_t id, Adafruit_PWMServoDriver* driver, limit_s limit) {
   this->limit = limit;
 }
 
+void Motor::setTarget(int8_t pos) {
+
+}
+
+void Motor::setUsePD(bool usePD) {
+
+}
+
+void Motor::setSpeed(float speed) {
+
+}
+
+void Motor::tick() {
+  
+
+  this->driver->setPWM(this->id, 0, this->position);
+}
+
+bool Motor::targetReached() {
+
+}
--- YASBot/YASBot.ino
+++ YASBot/YASBot.ino
@@ -1,21 +1,40 @@
+#include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
-#include "motor.h"
 
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-Motor test = Motor(0, &pwm);
+int position = 375;
+String fromComputer;
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println("Testing servo");
+
+  pwm.begin();
+
   //setup from Adafruit's example code
   pwm.setOscillatorFrequency(27000000); //I need an oscilloscope to set this correctly apparently (?????) buut I don't have one so we're leaving it to default and praying :)
   pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
 
   delay(10); //idk if this is necessary but it's in the example code and won't cause much disruption soooooo we're keeping it (if it aint broke)
-
-
 }
 
 void loop() {
+  fromComputer = Serial.readString();
 
+  // #define DEFAULT_MOTOR_MAX 625
+  // #define DEFAULT_MOTOR_MIN 125
+  if(fromComputer != "") {
+    if(fromComputer[0] == 'u')
+      position++;
+    else if(fromComputer[0] == 'd')
+      position--;
+    else
+      position = fromComputer.toInt();
+
+    Serial.println((String)position);
+  }
+  
+  pwm.setPWM(11, 0, position);
 }
